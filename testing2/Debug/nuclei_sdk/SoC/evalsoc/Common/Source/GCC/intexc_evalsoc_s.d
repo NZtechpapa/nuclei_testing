c6feb3a3677085f683a3b51f34a1397a65d62ac2
fred lei <suntearinheart@hotmail.com>
2024-05-21 11:37:22 +0800

    backup the codes

--- testing2/Debug/nuclei_sdk/SoC/evalsoc/Common/Source/GCC/intexc_evalsoc_s.d
+++ testing2/Debug/nuclei_sdk/SoC/evalsoc/Common/Source/GCC/intexc_evalsoc_s.d
@@ -1,6 +1,6 @@
 nuclei_sdk/SoC/evalsoc/Common/Source/GCC/intexc_evalsoc_s.o: \
  ../nuclei_sdk/SoC/evalsoc/Common/Source/GCC/intexc_evalsoc_s.S \
- D:\codes\jianash\gn001\testing2\nuclei_sdk\NMSIS\Core\Include/riscv_encoding.h \
- D:\codes\jianash\gn001\testing2\nuclei_sdk\NMSIS\Core\Include/riscv_bits.h
-D:\codes\jianash\gn001\testing2\nuclei_sdk\NMSIS\Core\Include/riscv_encoding.h:
-D:\codes\jianash\gn001\testing2\nuclei_sdk\NMSIS\Core\Include/riscv_bits.h:
+ D:\codes\opensource\nuclei_testing\testing2\nuclei_sdk\NMSIS\Core\Include/riscv_encoding.h \
+ D:\codes\opensource\nuclei_testing\testing2\nuclei_sdk\NMSIS\Core\Include/riscv_bits.h
+D:\codes\opensource\nuclei_testing\testing2\nuclei_sdk\NMSIS\Core\Include/riscv_encoding.h:
+D:\codes\opensource\nuclei_testing\testing2\nuclei_sdk\NMSIS\Core\Include/riscv_bits.h:
--- testing2/Debug/nuclei_sdk/SoC/evalsoc/Common/Source/GCC/startup_evalsoc.d
+++ testing2/Debug/nuclei_sdk/SoC/evalsoc/Common/Source/GCC/startup_evalsoc.d
@@ -1,6 +1,6 @@
 nuclei_sdk/SoC/evalsoc/Common/Source/GCC/startup_evalsoc.o: \
  ../nuclei_sdk/SoC/evalsoc/Common/Source/GCC/startup_evalsoc.S \
- D:\codes\jianash\gn001\testing2\nuclei_sdk\NMSIS\Core\Include/riscv_encoding.h \
- D:\codes\jianash\gn001\testing2\nuclei_sdk\NMSIS\Core\Include/riscv_bits.h
-D:\codes\jianash\gn001\testing2\nuclei_sdk\NMSIS\Core\Include/riscv_encoding.h:
-D:\codes\jianash\gn001\testing2\nuclei_sdk\NMSIS\Core\Include/riscv_bits.h:
+ D:\codes\opensource\nuclei_testing\testing2\nuclei_sdk\NMSIS\Core\Include/riscv_encoding.h \
+ D:\codes\opensource\nuclei_testing\testing2\nuclei_sdk\NMSIS\Core\Include/riscv_bits.h
+D:\codes\opensource\nuclei_testing\testing2\nuclei_sdk\NMSIS\Core\Include/riscv_encoding.h:
+D:\codes\opensource\nuclei_testing\testing2\nuclei_sdk\NMSIS\Core\Include/riscv_bits.h:
